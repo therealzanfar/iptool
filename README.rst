6f48fa0e291b5093977ff2fcf19e0fd511dc874e
Matthew Wyant <me@zanfar.com>
2022-05-05 11:46:12 -0700

    Initial project structure

---
+++ README.rst
@@ -0,0 +1,19 @@
+IPTool
+=============================
+
+IP Subnetting Tools
+
+Installation
+------------
+
+::
+
+   pip install git+https://github.com/therealzanfar/iptool.git
+
+Credits
+-------
+
+This package was created with
+`Cookiecutter <https://github.com/audreyr/cookiecutter>`__ and the
+`zanfar/cookiecutter-pypackage <https://gitlab.com/zanfar/cookiecutter-pypackage>`__
+project template.
---
+++ iptool/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2022 Nextiva, Inc.
+# All rights reserved.
+
+# cSpell:words Wyant
+
+"""IP Subnetting Tools"""
+
+__author__ = """Matthew Wyant"""
+__email__ = "me@matthewwyant.com"
+__copyright__ = "Copyright 2022, Matthew Wyant"
+__credits__ = [__author__]
+__license__ = "GPL-3.0-only"
+__version__ = "0.1.0"
+__version_info__ = (0, 1, 0)
+__maintainer__ = __author__
+__status__ = "Prototype"
