129f83dacd6cebe9b8545a23fa60c14d0bd4390a
Matthew Wyant <matthew.wyant@nextiva.com>
2023-10-31 14:42:29 -0700

    Improve logging setup to handle multiple calls

--- iptool/cli.py
+++ iptool/cli.py
@@ -22,13 +22,16 @@ RICH_THEME = Theme(
 rprint = Console(theme=RICH_THEME).print
 
 
-def setup_logging(verbosity: int = 0) -> None:
+def setup_logging(verbosity: int = 0, force: bool = False) -> None:
     """
     Set up a root logger with console output.
 
     Args:
         verbosity (int, optional): The logging level; 0=Error, 1=Warning,
             2=Info, 3+=Debug. Defaults to 0.
+
+        force (bool, optiona): Force the logging level to this value, even
+            if it's currently set to a more permissive value.
     """
     logging_level = logging.ERROR
     if verbosity == 1:
@@ -38,9 +41,26 @@ def setup_logging(verbosity: int = 0) -> None:
     elif verbosity >= 3:  # noqa: PLR2004
         logging_level = logging.DEBUG
 
-    logging.basicConfig(
-        level=logging_level,
-        format="%(message)s",
-        datefmt="[%x]",
-        handlers=[RichHandler(rich_tracebacks=True)],
-    )
+    if len(logging.root.handlers) == 0:
+        logging.basicConfig(
+            level=logging_level,
+            format="%(message)s",
+            datefmt="[%x]",
+            handlers=[RichHandler(rich_tracebacks=True)],
+        )
+
+        logger = logging.getLogger(__name__)
+        logger.info(f"Logging Setup at {logging.getLevelName(logging_level)} level")
+
+    elif logging.root.level > logging_level or force:
+        logging.root.setLevel(logging_level)
+
+        logger = logging.getLogger(__name__)
+        logger.info(f"Logging updated to {logging.getLevelName(logging_level)} level")
+
+    else:
+        logger = logging.getLogger(__name__)
+        logger.debug(
+            "Ignoring logging setup request: "
+            "handler already exists at desired or greater level",
+        )
